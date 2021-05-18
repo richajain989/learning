e3eb9dd34336010ed8cfdfd4609e5d6d80d6e11d
Kshitij Jain <ksjain@microsoft.com>
2021-05-19 00:44:37 +0530

    function with loop

--- loop.js
+++ loop.js
@@ -1,8 +1,30 @@
+//progrram without function
+// var x = 0;
+// for(i=1; i<=10; i++){
+//   var y=13;
+//   x = y*i;
+//   console.log(y,"*",i,"=" ,x);
+             
+// }
 
-var x = 0;
+//PROGRAMME WITH FUNCTION
+// function loop(){
+//     var x = 0;
+// for(i=1; i<=10; i++){
+//   var y=13;
+//   x = y*i;
+//   console.log(y,"*",i,"=" ,x);
+// }
+// }
+// loop()
+
+//PROGRAMME WITH FUNCTION WITH DYNAMIC PARAMETER
+function loop(y){
+    var x = 0;
 for(i=1; i<=10; i++){
-  var y=13;
+  //var y=13;
   x = y*i;
   console.log(y,"*",i,"=" ,x);
-              
 }
+}
+loop(5)
