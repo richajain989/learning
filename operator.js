6c5b42dd833327495bc215cf4aafe188ed0abf15
Kshitij Jain <ksjain@microsoft.com>
2021-05-21 02:05:03 +0530

    added pow and divide in oprator

--- operator.js
+++ operator.js
@@ -44,17 +44,53 @@ write a program of divide in which x is the divident and y is the divisor,
 and put a condition where y should be greater than 0. and if y =0 then 
 print "enter the value greater than 0"*/
 
-function divd(l,m){
-var p = l/m;
-if(m==0){
-    console.log("Please enter the value greater then zero");
-}
-else{
-    console.log(p)
-}
-}
+// function divd(l,m){
+// var p = l/m;
+// if(m==0){
+//     console.log("Please enter the value greater then zero");
+// }
+// else{
+//     console.log(p)
+// }
+// }
 
-divd(16,4)
-divd(35,7)
-divd(12,0)
+// divd(16,4)
+// divd(35,7)
+// divd(12,0)
 
+// write a program of divide in which a is the divident and b is the divisor, 
+// and put a condition where b should be greater than 0. and if b =0 then 
+// print "enter the value greater than 0"*/
+
+// function divd(a,b){
+//     if(b==0){
+//         console.log("enter thevalue greater then 0");
+//     }
+//     else{
+//         let c =a/b;
+//         console.log(c)
+//     }
+    
+// }
+
+// divd(12,2)//6
+// divd(12,0)//err
+// divd(13,1)//13
+// divd(5,2)//2.5
+// divd(7,0)//err
+
+// agar zero hua to -print error MSG
+// agar nahi hua to 
+// divide them
+// then print
+
+//FUNCTION POW-
+function power(p,q){
+    let r=p**q;
+    console.log(r);
+}
+power(2,5)
+
+// needs two value
+// 2 ki power 5
+// print
